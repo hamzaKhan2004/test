f4715c463aeb9063dbe312704fce7fca7c216058
SDAC-8 <SDAC-8@SDAC-8-PC>
2024-06-26 14:31:05 -0700

    Second main class "1.0.1"

---
+++ Recap/src/Main2.java
@@ -0,0 +1,19 @@
+class Override{
+	void meth1(int a,int b){
+		System.out.println(a+b);
+	}
+}
+class B extends Override{
+	
+	void meth1(int a, int b) {
+		System.out.println(a-b);
+	}
+}
+
+public class Main2 {
+	public static void main(String[] args) {
+		System.out.println("1.0.1");
+		B obj2 = new B();
+		obj2.meth1(30, 10);
+	}
+}
